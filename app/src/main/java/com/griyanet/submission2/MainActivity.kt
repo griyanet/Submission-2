c2c8b8a05caf2cc6465571c1e1a312b351b7a3d3
griyanet <griyanetcafe@gmail.com>
2020-11-15 15:45:39 +0800

    error 1

--- app/src/main/java/com/griyanet/submission2/MainActivity.kt
+++ app/src/main/java/com/griyanet/submission2/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.griyanet.submission2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.util.Log
-import androidx.lifecycle.Observer
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.get
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.griyanet.submission2.Model.Repository
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var countdownTimer: CountDownTimer
+    private var seconds = 3L
     private lateinit var viewmodel: MainViewModel
     private val myAdapter by lazy {UserAdapter()}
 
@@ -17,15 +22,67 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        submitRequest()
+    }
+
+    private fun submitRequest() {
+        showLoading(true)
+        setupRecyclerView()
         val repository = Repository()
         val viewModelFactory = MainViewModelFactory(repository)
         viewmodel = ViewModelProvider(this, viewModelFactory).get(MainViewModel::class.java)
         viewmodel.getUser()
-        viewmodel.myResponse.observe(this, Observer { response ->
-            Log.d("Response", response.body()?.)
+        viewmodel.usersResponse.observe(this, { response ->
+            if (response.isSuccessful) {
+                fadeIn()
+                showLoading(false)
+                response.body()?.let { myAdapter.setData(it) }
+            } else {
+                Toast.makeText(this, response.code(), Toast.LENGTH_SHORT).show()
+                Log.d("MainActivity", "Something wrong..... response code: ${response.code()}")
+                showLoading(true)
+                attemptReqAgain(seconds)
+            }
         })
-
-
-
     }
+
+    private fun setupRecyclerView() {
+        rv_user.setHasFixedSize(true)
+        rv_user.adapter = myAdapter
+        rv_user.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun fadeIn() {
+        v_blackScreen.animate().apply {
+            alpha(0f)
+            duration = 3000
+        }.start()
+    }
+
+    private fun attemptReqAgain(seconds: Long) {
+        countdownTimer = object: CountDownTimer(seconds*1010, 1000) {
+            override fun onFinish() {
+                submitRequest()
+                countdownTimer.cancel()
+                tv_noInternetCountDown.visibility = View.GONE
+                this@MainActivity.seconds+=3
+            }
+
+            override fun onTick(milsecTilFinish: Long) {
+                tv_noInternetCountDown.visibility = View.VISIBLE
+                tv_noInternetCountDown.text = getString(R.string.no_Internet)
+                Log.d("MainActivity", "Could not retrieve data. Trying again in: ${milsecTilFinish/1000} seconds")
+            }
+        }
+        countdownTimer.start()
+    }
+
+    private fun showLoading(state: Boolean) {
+        if (state) {
+            progressBar.visibility = View.VISIBLE
+        } else {
+            progressBar.visibility = View.GONE
+        }
+    }
+
 }
