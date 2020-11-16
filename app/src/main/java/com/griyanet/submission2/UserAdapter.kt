bbe8268e1976e4ec91bde50ccbc8d9e00a5e5ea8
griyanet <griyanetcafe@gmail.com>
2020-11-16 22:57:12 +0800

    another update

--- app/src/main/java/com/griyanet/submission2/UserAdapter.kt
+++ app/src/main/java/com/griyanet/submission2/UserAdapter.kt
@@ -3,27 +3,35 @@ package com.griyanet.submission2
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Adapter
 import androidx.recyclerview.widget.RecyclerView
 import com.griyanet.submission2.Model.UserItem
+import kotlinx.android.synthetic.main.row_itemuser.view.*
 
 class UserAdapter : RecyclerView.Adapter<UserAdapter.MyViewHolder>() {
 
     private var userList = ArrayList<UserItem>()
 
     inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
+        fun userBind(userItem: UserItem) {
+            with(itemView) {
+                tv_login.text = userItem.login
+                tv_userUrl.text = userItem.url
+                tv_followerUrl.text = userItem.followersUrl
+                tv_followingUrl.text = userItem.followingUrl
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        return MyViewHolder(LayoutInflater.from(parent.context).inflate(R.layout))
+        return MyViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.row_itemuser, parent, false)
+        )
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.userBind(userList[position])
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = userList.size
+
 }
