a2a4ab7b79bb76b7af784ae481fcb0e0540f343d
griyanet <griyanetcafe@gmail.com>
2020-11-16 22:10:08 +0800

    another update

---
+++ app/src/main/java/com/griyanet/submission2/UserAdapter.kt
@@ -0,0 +1,29 @@
+package com.griyanet.submission2
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Adapter
+import androidx.recyclerview.widget.RecyclerView
+import com.griyanet.submission2.Model.UserItem
+
+class UserAdapter : RecyclerView.Adapter<UserAdapter.MyViewHolder>() {
+
+    private var userList = ArrayList<UserItem>()
+
+    inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        return MyViewHolder(LayoutInflater.from(parent.context).inflate(R.layout))
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
